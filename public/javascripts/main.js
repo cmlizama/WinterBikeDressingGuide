e0bb674c475d5dc52a4754cc4e2f02161ca36e4e
Chris Lizama <christopher.lizama@gmail.com>
2013-11-13 12:15:09 -0700

    added basic functionaly to pull in a couple of instagram #biking photos

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -86,7 +86,44 @@ $(function(){
     });
 
 
+    var tag = "biking";
+    var count = 2;
+    var access_token = '22033045.ea9028a.eec94286a2e049429fe51c3fbc95db20';
+    var access_parameters = {access_token:access_token}
+
+    function grabImages(access_parameters) {
+
+        var instagramUrl = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?callback=?&count='+ count;
+
+        $.getJSON(instagramUrl, access_parameters, onDataLoaded);
+    }
 
 
+    function onDataLoaded(instagram_data) {
+
+      if(instagram_data.meta.code == 200) {
+
+        var photos = instagram_data.data;
+
+
+        if(photos.length > 0) {
+
+          for (var key in photos){
+                var photo = photos[key];
+                $('#target').append('<img src="'+photo.images.thumbnail.url+'">');
+          }
+        } else {
+
+          $('#target').append('Hmm.  I could not find anything!');
+            }
+        } else {
+
+          var error = data.meta.error_message;
+          $('#target').append('Something happened, Instagram said: ' + error);
+        }
+      }
+    
+
+    grabImages(access_parameters);
 
 });
